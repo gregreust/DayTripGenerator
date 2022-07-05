b9fe8c5211228f03a47f5f0538ed5edc13429493
GregReust <greg.reust@gmail.com>
2022-07-05 16:30:59 -0500

    Defined the available options using lists

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DayTripGenerator
-Randomly select an agenda for a fun day trip
+Randomly select an agenda for a fun day trip.
+Week 2 project for DevCodeCamp, created by Greg Reust
---
+++ main.py
@@ -0,0 +1,17 @@
+from random import choice
+
+#destination options
+destinations = ['Austin', 'New Orleans', 'San Antonio', 'Chicago', 'Atlanta', 'Santa Fe', 'Nashville', 'Washington DC']
+#transportation options
+transportations = ['plane', 'train', 'rental car', 'bus', 'scooter', 'horse', 'bicycle']
+#entertainment options
+entertainments = ['concert', 'art museum', 'bar crawl', 'shopping spree', 'guided tour', 'cooking class', 'zoo', 'farmer\'s market']
+#dining options
+austin_dining = ['Odd Duck', 'Launderette', 'ATX Cocina', 'Salty Sow', 'Jeffery\'s']
+new_orleans_dining = ['Cochon', 'GW Fins', 'Commander\'s Palace', 'Peche Seafood Grill', 'Brennan\'s']
+san_antonio_dining = ['Southerleigh', 'Fogo de Chão', 'Bliss', 'Bohanan\'s Prime Steaks and Seafood', 'Cappy\'s']
+chicago_dining = ['The Purple Pig', 'Girl & The Goat', 'Boka', 'Monteverde', 'Alinea']
+atlanta_dining = ['Poor Calvin\'s', 'South City Kitchen', 'Nan Thai', 'Lazy Betty', 'Sotto Sotto']
+santa_fe_dining = ['The Shed', 'Marker Steer Steakhouse', 'Radish & Rye', 'Sassella', 'La Boca']
+nashville_dining = ['Butcher & Bee', 'Etch', 'The Catbird Seat', 'Adele\'s', 'Kayne Prime Steakhouse']
+washington_dc_dining = ['Estadio', 'The Dabney', 'Le Diplomate', 'Fiola Mare', 'Founding Farmers']
